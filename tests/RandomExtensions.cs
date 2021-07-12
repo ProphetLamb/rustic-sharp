d49fee225792e7a932a7b7ccabc85d7d190902a6
ProphetLamb <rene.carannante@outlook.de>
2021-07-12 23:25:19 +0200

    Add test cases

---
+++ tests/RandomExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeaplessUtility.Tests
+{
+    public static class RandomExtensions
+    {
+        public static ref T ChooseFrom<T>(this Random random, T[] collection)
+        {
+            return ref collection[random.Next(0, collection.Length)];
+        }
+    }
+}
