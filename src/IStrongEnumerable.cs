acd613d9061aafc536c1a2bb681bf70160fb83ec
ProphetLamb <rene.carannante@outlook.de>
2021-08-02 15:21:49 +0200

    Inherit from IEnumerable<T>

--- src/IStrongEnumerable.cs
+++ src/IStrongEnumerable.cs
@@ -2,7 +2,7 @@
 
 namespace HeaplessUtility
 {
-    public interface IStrongEnumerable<TElement, out TEnumerator>
+    public interface IStrongEnumerable<TElement, out TEnumerator> : IEnumerable<TElement>
         where TEnumerator : IEnumerator<TElement>
     {
         TEnumerator GetEnumerator();
