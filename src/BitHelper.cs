edd67748fc5eb17bff3cb0c8e30b30156381e6ab
ProphetLamb <rene.carannante@outlook.de>
2022-01-23 22:22:29 +0100

    Seperate BitMarker from BitHelper
    - BirMarker allows marking bit positions with a boolean value.
    - BitHelper contains (extension) methods for various sw implementations of bithacks.

--- src/BitHelper.cs
+++ src/BitHelper.cs
@@ -5,8 +5,7 @@ using System.Runtime.InteropServices;
 
 namespace HeaplessUtility
 {
-    [CLSCompliant(false)]
-    public ref struct BitHelper
+    public static class BitHelper
     {
         private static ReadOnlySpan<ulong> DigitCountTable => new ulong[32]
         {
@@ -19,7 +18,7 @@ namespace HeaplessUtility
             42949672960, 42949672960
         };
 
-        private static ReadOnlySpan<byte> DeBruijnSequenceLog2 => new byte[32]
+        private static ReadOnlySpan<byte> MultiplyDeBruijnBitPosition2 => new byte[32]
         {
             00, 09, 01, 10, 13, 21, 02, 29,
             11, 14, 16, 18, 22, 25, 03, 30,
@@ -33,12 +32,12 @@ namespace HeaplessUtility
             31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
         };
 
-        private static ReadOnlySpan<int> Int32Power10 => new int[10]
+        private static ReadOnlySpan<uint> Int32Power10 => new uint[10]
         {
             1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000
         };
 
-        private static ReadOnlySpan<long> Int64Power10 => new long[19]
+        private static ReadOnlySpan<ulong> Int64Power10 => new ulong[19]
         {
             1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000, 100000000000, 1000000000000,
             10000000000000, 100000000000000, 1000000000000000, 10000000000000000, 100000000000000000, 1000000000000000000
@@ -47,57 +46,6 @@ namespace HeaplessUtility
         private const int IntSize = 32;
         private const int IntShift = 5;
 
-        private readonly Span<int> _span;
-
-        public BitHelper(Span<int> span, bool clear)
-        {
-            if (clear)
-            {
-                span.Clear();
-            }
-            _span = span;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void MarkBit(int bitPosition)
-        {
-            int bitArrayIndex = bitPosition >> IntShift;
-            if ((uint)bitArrayIndex < (uint)_span.Length)
-            {
-                _span[bitArrayIndex] |= 1 << FastModulo2(bitPosition, IntSize);
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool ToggleBit(int bitPosition)
-        {
-            int bitArrayIndex = bitPosition >> IntShift;
-            if ((uint)bitArrayIndex < (uint)_span.Length)
-            {
-                return (_span[bitArrayIndex] ^= 1 << FastModulo2(bitPosition, IntSize)) != 0;
-            }
-            return false;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void SetBit(int bitPosition, int value)
-        {
-            Debug.Assert((value & 1) == value, "Value must be either zero or one, false or true respectively.");
-            int bitArrayIndex = bitPosition >> IntShift;
-            int mask = 1 << FastModulo2(bitPosition, IntSize);
-            if ((uint)bitArrayIndex < (uint)_span.Length)
-            {
-                _span[bitArrayIndex] = (_span[bitArrayIndex] & ~mask) | (_span[bitArrayIndex] & (mask * value));
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool IsMarked(int bitPosition)
-        {
-            int bitArrayIndex = bitPosition >> IntShift;
-            return (uint)bitArrayIndex < (uint)_span.Length && (_span[bitArrayIndex] & (1 << (bitPosition % IntSize))) != 0;
-        }
-
         /// <summary>
         ///     How many ints must be allocated to represent n bits. Returns (n+31)/32, but avoids overflow.
         /// </summary>
@@ -105,84 +53,105 @@ namespace HeaplessUtility
         public static int ToIntArrayLength(int n) => n > 0 ? (n - 1) / IntSize + 1 : 0;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Negate(int value) => (value ^ -1) + 1;
+        public static int Negate(this int value) => (value ^ -1) + 1;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static long Negate(long value) => (value ^ -1) + 1;
+        public static long Negate(this long value) => (value ^ -1) + 1;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int NegateIf(int negate, int value)
+        public static int NegateIf(this int negate, int value)
         {
             Debug.Assert((negate & 1) == negate);
             return (value ^ -negate) + negate;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static long NegateIf(long negate, long value)
+        public static long NegateIf(this long negate, long value)
         {
             Debug.Assert((negate & 1) == negate);
             return (value ^ -negate) + negate;
         }
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint FastRange(uint value, uint maximum)
+        public static uint FastRange(this uint value, uint maximum)
         {
             return (uint)(((ulong)value * (ulong)maximum) >> 32);
         }
 
+        /// <summary>Returns approximate reciprocal of the divisor: ceil(2**64 / divisor).</summary>
+        /// <remarks>This should only be used on 64-bit.</remarks>
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong FastModuloMultiplier(uint divisor)
+        public static ulong GetFastModMultiplier(uint divisor)
         {
             return 0xFFFFFFFFFFFFFFFFul / divisor + 1;
         }
 
+        /// <summary>Performs a mod operation using the multiplier pre-computed with <see cref="GetFastModMultiplier"/>.</summary>
+        /// <remarks>This should only be used on 64-bit.</remarks>
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint FastModulo(uint value, uint divisor, ulong multiplier)
+        public static uint FastMod(uint value, uint divisor, ulong multiplier)
         {
+            Debug.Assert(IntPtr.Size == 8);
+            // We use modified Daniel Lemire's fastmod algorithm (https://github.com/dotnet/runtime/pull/406),
+            // which allows to avoid the long multiplication if the divisor is less than 2**31.
             Debug.Assert(divisor <= Int32.MaxValue);
 
+            // This is equivalent of (uint)Math.BigMul(multiplier * value, divisor, out _). This version
+            // is faster than BigMul currently because we only need the high bits.
             uint hi = (uint)(((((multiplier * value) >> 32) + 1) * divisor) >> 32);
 
             Debug.Assert(hi == value % divisor);
             return hi;
         }
 
+        /// <summary>Performs a mod operation on a 32bit signed integer where the divisor is a multiple of 2.</summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int FastModulo2(int value, int divisor)
+        public static int FastMod2(this int value, int divisor)
         {
             Debug.Assert((divisor & 1) == 0);
             return value & (divisor - 1);
         }
 
+        /// <summary>Performs a mod operation on a 32bit unsigned integer where the divisor is a multiple of 2.</summary>
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint FastModulo2(uint value, uint divisor)
+        public static uint FastMod2(this uint value, uint divisor)
         {
             Debug.Assert((divisor & 1) == 0);
             return value & (divisor - 1);
         }
 
+        /// <summary>Performs a mod operation on a 64bit signed integer where the divisor is a multiple of 2.</summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static long FastModulo2(long value, long divisor)
+        public static long FastMod2(this long value, long divisor)
         {
             Debug.Assert((divisor & 1) == 0);
             return value & (divisor - 1);
         }
 
+        /// <summary>Performs a mod operation on a 64bit unsigned integer where the divisor is a multiple of 2.</summary>
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong FastModulo2(ulong value, ulong divisor)
+        public static ulong FastMod2(this ulong value, ulong divisor)
         {
             Debug.Assert((divisor & 1) == 0);
             return value & (divisor - 1);
         }
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint HasAlignedZeroByte(uint value) => (value - 0x01010101u) & ~value & 0x80808080u;
+        public static uint HasAlignedZeroByte(this uint value) => (value - 0x01010101u) & ~value & 0x80808080u;
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong HasAlignedZeroByte(ulong value) => (value - 0x0101010101010101ul) & ~value & 0x8080808080808080ul;
+        public static ulong HasAlignedZeroByte(this ulong value) => (value - 0x0101010101010101ul) & ~value & 0x8080808080808080ul;
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong Mix(ulong key)
+        public static ulong Mix(this ulong key)
         {
             key ^= key >> 33;
             key *= 0xff51afd7ed558ccd;
@@ -194,54 +163,116 @@ namespace HeaplessUtility
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Max(int x, int y)
+        public static int Max(this int x, int y)
         {
             Debug.Assert(Math.Abs(x) == x && Math.Abs(y) == y);
             return x - ((x - y) & ((x - y) >> 31));
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Min(int x, int y)
+        public static int Min(this int x, int y)
         {
             Debug.Assert(Math.Abs(x) == x && Math.Abs(y) == y);
             return y + ((x - y) & ((x - y) >> 31));
         }
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static long Max(long x, long y)
+        public static uint Max(this uint x, uint y)
         {
             Debug.Assert(Math.Abs(x) == x && Math.Abs(y) == y);
             return x - ((x - y) & ((x - y) >> 31));
         }
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static long Min(long x, long y)
+        public static uint Min(this uint x, uint y)
         {
             Debug.Assert(Math.Abs(x) == x && Math.Abs(y) == y);
             return y + ((x - y) & ((x - y) >> 31));
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int DigitCount(uint value)
+        public static long Max(this long x, long y)
         {
-            return (int)((value + DigitCountTable[Log2(value)]) >> 32);
+            Debug.Assert(Math.Abs(x) == x && Math.Abs(y) == y);
+            return x - ((x - y) & ((x - y) >> 63));
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Log2(uint value)
+        public static long Min(this long x, long y)
         {
-            return DeBruijnSequenceLog2[(int)((FillTailingZeros(value) * 0x07C4ACDDu) >> 27)];
+            Debug.Assert(Math.Abs(x) == x && Math.Abs(y) == y);
+            return y + ((x - y) & ((x - y) >> 63));
         }
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Log10(uint value)
+        public static ulong Max(this ulong x, ulong y)
+        {
+            return x - ((x - y) & ((x - y) >> 63));
+        }
+
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong Min(this ulong x, ulong y)
+        {
+            return y + ((x - y) & ((x - y) >> 63));
+        }
+
+        /// <summary>Computes the number of decimal digits required to represent the integer value.</summary>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int DigitCount(this uint value)
+        {
+            return (int)((value + Unsafe.AddByteOffset(ref MemoryMarshal.GetReference(DigitCountTable), (IntPtr)Log2(value))) >> 32);
+        }
+
+        /// <summary>Computes the base 10 logarithm of on a 64bit unsigned integer value.</summary>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Log10(this uint value)
         {
             int l2 = (Log2(value) + 1) * 1233 >> 12;
             return l2 - (value < Int32Power10[l2] ? 1 : 0);
         }
 
+        /// <summary>Computes the base 10 logarithm of on a 64bit unsigned integer value.</summary>
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint RoundUpToEven(uint value)
+        public static int Log10(this ulong value)
+        {
+            int l2 = (Log2(value) + 1) * 1233 >> 12;
+            return l2 - (value < Int64Power10[l2] ? 1 : 0);
+        }
+
+        /// <summary>Performs a base 2 logarithm operation on an integer using a LUT.</summary>
+        /// <remarks>The value is floored to the next lowest multiple of two from the value, i.e. Log2Floor(3) == Log2(2), and Log2Floor(16) == Log2(16).</remarks>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Log2(this uint value)
+        {
+            return Unsafe.AddByteOffset(ref MemoryMarshal.GetReference(MultiplyDeBruijnBitPosition2), (IntPtr)(int)(((value & 0xFFFFFFFE) * 0x07C4ACDDU) >> 27));
+        }
+
+        /// <summary>Performs a base 2 logarithm operation on an integer using a LUT.</summary>
+        /// <remarks>The value is floored to the next lowest multiple of two from the value, i.e. Log2Floor(3) == Log2(2), and Log2Floor(16) == Log2(16).</remarks>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Log2(this ulong value)
+        {
+            uint hi = (uint)(value >> 32);
+            if (hi == 0) // Log2 < 32 ?
+            {
+                return Log2((uint)value);
+            }
+
+            return 32 + Log2(hi);
+        }
+
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint RoundUpToEven(this uint value)
         {
             value--;
             value = FillTailingZeros(value);
@@ -250,8 +281,9 @@ namespace HeaplessUtility
             return value;
         }
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint FillTailingZeros(uint value)
+        public static uint FillTailingZeros(this uint value)
         {
             value |= value >> 1;
             value |= value >> 2;
@@ -262,8 +294,9 @@ namespace HeaplessUtility
             return value;
         }
 
+        [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int CountTrailingZeroBits(uint value)
+        public static int CountTrailingZeroBits(this uint value)
         {
             return DeBruijnSequenceBitPosition[(int)(uint)((value & -value) * 0x077CB531U) >> 27];
         }
--- src/BitMarker.cs
+++ src/BitMarker.cs
@@ -13,18 +13,7 @@ namespace HeaplessUtility
     public readonly ref struct BitMarker
     {
         private const int IntSize = sizeof(int) * 8;
-        private static readonly int IntShift = Log2Floor(IntSize);
-        private static ReadOnlySpan<int> MultiplyDeBruijnBitPosition => new[]
-        {
-            0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
-            8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
-        };
-        private static ReadOnlySpan<int> MultiplyDeBruijnBitPosition2 => new[]
-        {
-            0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
-            31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
-        };
-
+        private static readonly int IntShift = BitHelper.Log2(IntSize);
         private readonly Span<int> _span;
 
         /// <summary>
@@ -43,7 +32,7 @@ namespace HeaplessUtility
             int bitArrayIndex = bitIndex >> IntShift;
             if ((uint)bitArrayIndex < (uint)_span.Length)
             {
-                _span[bitArrayIndex] |= (1 << (int)FastMod2((uint)bitIndex, IntSize));
+                _span[bitArrayIndex] |= 1 << (int)((uint)bitIndex).FastMod2(IntSize);
             }
         }
 
@@ -54,7 +43,7 @@ namespace HeaplessUtility
             int bitArrayIndex = bitIndex >> IntShift;
             if ((uint)bitArrayIndex < (uint)_span.Length)
             {
-                _span[bitArrayIndex] &= ~(1 << (int)FastMod2((uint)bitIndex, IntSize));
+                _span[bitArrayIndex] &= ~(1 << (int)((uint)bitIndex).FastMod2(IntSize));
             }
         }
 
@@ -69,106 +58,6 @@ namespace HeaplessUtility
                 int mask = ~(1 << (bitIndex % IntSize));
                 _span[bitArrayIndex] = (_span[bitArrayIndex] & mask) | (value * ~mask);
             }
-        }
-
-        /// <summary>Returns approximate reciprocal of the divisor: ceil(2**64 / divisor).</summary>
-        /// <remarks>This should only be used on 64-bit.</remarks>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong GetFastModMultiplier(uint divisor)
-        {
-            Debug.Assert(IntPtr.Size == 8);
-            return ulong.MaxValue / divisor + 1;
-        }
-
-        /// <summary>Performs a mod operation using the multiplier pre-computed with <see cref="GetFastModMultiplier"/>.</summary>
-        /// <remarks>This should only be used on 64-bit.</remarks>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint FastMod(uint value, uint divisor, ulong multiplier)
-        {
-            Debug.Assert(IntPtr.Size == 8);
-            // We use modified Daniel Lemire's fastmod algorithm (https://github.com/dotnet/runtime/pull/406),
-            // which allows to avoid the long multiplication if the divisor is less than 2**31.
-            Debug.Assert(divisor <= int.MaxValue);
-
-            // This is equivalent of (uint)Math.BigMul(multiplier * value, divisor, out _). This version
-            // is faster than BigMul currently because we only need the high bits.
-            uint hi = (uint)(((((multiplier * value) >> 32) + 1) * divisor) >> 32);
-
-            Debug.Assert(hi == value % divisor);
-            return hi;
-        }
-
-        /// <summary>Performs a mod operation on a 32bit unsigned integer where the divisor is a multiple of 2.</summary>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint FastMod2(uint value, uint divisor)
-        {
-            Debug.Assert((divisor & 1) == 0, "The divisor has to be a multiple of 2.");
-            return value & (divisor - 1);
-        }
-
-        /// <summary>Performs a mod operation on a 64bit unsigned integer where the divisor is a multiple of 2.</summary>
-        /// <remarks>This should only be used on 64-bit.</remarks>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong FastMod2(ulong value, ulong divisor)
-        {
-            Debug.Assert(IntPtr.Size == 8);
-            Debug.Assert((divisor & 1) == 0, "The divisor has to be a multiple of 2.");
-            return value & (divisor - 1);
-        }
-
-        /// <summary>Performs a base 2 logarithm operation on an integer using a LUT.</summary>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Log2(uint value)
-        {
-            value |= value >> 1;
-            value |= value >> 2;
-            value |= value >> 4;
-            value |= value >> 8;
-            value |= value >> 16;
-            return Unsafe.AddByteOffset(ref MemoryMarshal.GetReference(MultiplyDeBruijnBitPosition), (IntPtr)(int)((value * 0x07C4ACDDU) >> 27));
-        }
-
-        /// <summary>Performs a base 2 logarithm operation on an integer using a LUT.</summary>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Log2(ulong value)
-        {
-            uint hi = (uint)(value >> 32);
-            if (hi == 0) // Log2 < 32 ?
-            {
-                return Log2((uint)value);
-            }
-
-            return 32 + Log2((uint)value);
-        }
-
-        /// <summary>Performs a base 2 logarithm operation on an integer using a LUT.</summary>
-        /// <remarks>The value is floored to the next lowest multiple of two from the value, i.e. Log2Floor(3) == Log2(2), and Log2Floor(16) == Log2(16).</remarks>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Log2Floor(uint value)
-        {
-            return Unsafe.AddByteOffset(ref MemoryMarshal.GetReference(MultiplyDeBruijnBitPosition2), (IntPtr)(int)(((value & 0xFFFFFFFE) * 0x07C4ACDDU) >> 27));
-        }
-
-        /// <summary>Performs a base 2 logarithm operation on an integer using a LUT.</summary>
-        /// <remarks>The value is floored to the next lowest multiple of two from the value, i.e. Log2Floor(3) == Log2(2), and Log2Floor(16) == Log2(16).</remarks>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Log2Floor(ulong value)
-        {
-            uint hi = (uint)(value >> 32);
-            if (hi == 0) // Log2 < 32 ?
-            {
-                return Log2Floor((uint)value);
-            }
-
-            return 32 + Log2Floor(hi);
         }
     }
 }
