6f66c49322d5dc620f8073a18ef268de70ef9882
ProphetLamb <rene.carannante@outlook.de>
2022-02-10 15:49:10 +0100

    Switch to nuget properties to build.props
    - normalize targetframework strings
    - xml doc generator

--- gendoc.ps1
+++ gendoc.ps1
@@ -1,7 +1,14 @@
-mkdir "doc/"
+mkdir -Force "doc/"
 
 foreach ($project in Get-ChildItem -Path src/ -Recurse -File *.csproj) {
     $dir=$project.DirectoryName
     $name=[System.IO.Path]::GetFileNameWithoutExtension($project.Name)
-    xmldoc2md "$dir/bin/Debug/net5.0/$name.dll" "doc/$name"
+
+    # default namespace
+    if ((Get-Content $project) -match "<DefaultNamespace>(.*)<DefaultNamespace/>")
+    {
+        $name=$Matches[0]
+    }
+
+    xmldoc2md "$dir/bin/Debug/net50/$name.dll" "doc/$name"
 }
