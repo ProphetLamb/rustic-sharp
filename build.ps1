aa121297ba658a08329628564a61e690907b57ce
ProphetLamb <rene.carannante@outlook.de>
2022-02-10 10:15:24 +0100

    Test all target frameworks

--- build.ps1
+++ build.ps1
@@ -1,27 +1,39 @@
-$Env:LABEL = "CI" + $Env:APPVEYOR_BUILD_NUMBER.PadLeft(5, "0")
+$Env:LABEL = if ($Env:APPVEYOR) { "CI" + $Env:APPVEYOR_BUILD_NUMBER.PadLeft(5, "0") } else { "local" }
 $Env:DOTNET_CLI_TELEMETRY_OPTOUT=1
 $Env:DOTNET_NOLOGO=1
 
-Write-Output "`n========================================================================================================================`nBuild and pack libs`n========================================================================================================================`n"
+$target_frameworks = [String[]] 'netstandard2.1', 'netcoreapp3.1', 'net5.0', 'net6.0'
+
+dotnet restore -verbosity:q
+
+echo "`n========================================================================================================================`n
+Build and pack libs
+`n========================================================================================================================`n"
 Push-Location .\src
 
-Get-ChildItem -Recurse Rustic*.csproj -File | ForEach-Object {
-    Push-Location $_.Directory
+foreach ($project in Get-ChildItem -Recurse Rustic*.csproj -File) {
+    Push-Location $project.Directory
 
-    dotnet restore -verbosity:q
+    echo "`n========================================================================================================================`nBuild $project"
     dotnet build -c:Release --no-restore -verbosity:q --version-suffix $Env:LABEL
-    dotnet pack -c:Release --no-restore -verbosity:q --version-suffix $Env:LABEL
+    dotnet pack -c:Release --no-restore --no-build -verbosity:q --version-suffix $Env:LABEL
 
     Pop-Location
 }
 Pop-Location
 
-Write-Output "`n========================================================================================================================`nRun and cover tests`n========================================================================================================================`n"
+echo "`n========================================================================================================================`n
+Run and cover tests
+`n========================================================================================================================`n"
 Push-Location .\tests
-Get-ChildItem -Recurse Rustic*Tests.csproj -File | ForEach-Object {
-    Push-Location $_.Directory
 
-    dotnet test -verbosity:q /p:AltCover=true
+foreach ($project in Get-ChildItem -Recurse Rustic*Tests.csproj -File) {
+    Push-Location $project.Directory
+
+    echo "`n========================================================================================================================`nTest $project"
+    foreach ($target in $target_frameworks) {
+        dotnet test --framework:$target -verbosity:q /p:AltCover=true
+    }
 
     Pop-Location
 }
