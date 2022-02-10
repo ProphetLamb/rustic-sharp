239ba8b4502cc2688d3562ce422961a9ec79ea13
ProphetLamb <rene.carannante@outlook.de>
2022-02-11 00:17:14 +0100

    Filter coverage
    - Picks native testing assemblies
    - Uses absolute paths

--- coverage.ps1
+++ coverage.ps1
@@ -1,10 +1,11 @@
 # We need to run the tests again, bc Appveyor doesnt use dotnet cli for testing, and I cant get the AppveyorTestlogger to work.
-& dotnet test /p:AltCover="true"
+dotnet test --no-restore --no-build /p:AltCover="true" /p:AltCoverAssemblyFilter="Rustic\..*" /p:AltCoverAssemblyExcludeFilter=".*\.Tests"
 
 $commitID = & git rev-parse HEAD
 $commitBranch = & git rev-parse --abbrev-ref HEAD
 
 foreach ($coverage in Get-ChildItem -Recurse -File coverage*.xml)
 {
-    & csmacnz.coveralls --opencover -i $coverage.FullName --useRelativePaths --basePath $pwd --commitId $commitID --commitBranch $commitBranch
+    $relative=Resolve-Path -Relative $coverage.FullName
+    & csmacnz.coveralls --opencover -i $relative --useRelativePaths --basePath $pwd --commitId $commitID --commitBranch $commitBranch
 }
