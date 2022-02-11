2beef534e28069209f853b45a37b8995af516818
ProphetLamb <prophet.lamb@gmail.com>
2022-02-11 16:43:19 +0100

    Target net48 instead of netcore31
    - netcore31 has minimal differences to netstandard21 form the perspective of the project.
      However net48 is a different net repository to support.
    - Strict filter for code coverage

--- coverage.ps1
+++ coverage.ps1
@@ -1,5 +1,5 @@
 # We need to run the tests again, bc Appveyor doesnt use dotnet cli for testing, and I cant get the AppveyorTestlogger to work.
-dotnet test --no-restore --no-build -verbosity:m /p:AltCover="true" /p:AltCoverAssemblyExcludeFilter="NUnit|AltCover"
+dotnet test --no-restore --no-build -verbosity:m /p:AltCover="true" /p:AltCoverAssemblyExcludeFilter="NUnit|AltCover" /p:AltCoverAssemblyFilter="Rustic\."
 
 $commitID = & git rev-parse HEAD
 $commitBranch = & git rev-parse --abbrev-ref HEAD
