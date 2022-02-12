6804d4da586a3635341526bd0c6e477d58a7877d
ProphetLamb <rene.carannante@outlook.de>
2022-02-12 19:50:32 +0100

    Fix coverage includes Nunit source

--- test.ps1
+++ test.ps1
@@ -1,10 +1,10 @@
-dotnet test --no-restore --no-build -verbosity:m  --test-adapter-path:. --logger:Appveyor /p:AltCover="true" /p:AltCoverAssemblyExcludeFilter=".*\.Tests" /p:AltCoverAssemblyFilter="Rustic\..*"
+dotnet test --no-restore --no-build --test-adapter-path:. --logger:Appveyor /p:AltCover="true"
 
 $commitID = & git rev-parse HEAD
 $commitBranch = & git rev-parse --abbrev-ref HEAD
 
 foreach ($coverage in Get-ChildItem -Recurse -File coverage*.xml)
 {
-    $relative=Resolve-Path -Relative $coverage.FullName
+    $relative=Resolve-Path -Relative $coverage
     & csmacnz.coveralls --opencover -i $relative --useRelativePaths --basePath $pwd --commitId $commitID --commitBranch $commitBranch
 }
