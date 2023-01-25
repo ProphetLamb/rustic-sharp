4d6e51b9f66db4991d2c3e3a3f2d9bc693935dd5
Prophet Lamb <prophet.lamb@gmail.com>
2023-01-25 14:36:36 +0100

    Use file-scoped-namespaces

--- tests/Text/SplitIterTests.cs
+++ tests/Text/SplitIterTests.cs
@@ -5,75 +5,74 @@ using NUnit.Framework;
 
 using Rustic.Text;
 
-namespace Rustic.Text.Tests
+namespace Rustic.Text.Tests;
+
+[TestFixture]
+public class SplitIterTests
 {
-    [TestFixture]
-    public class SplitIterTests
+    public static readonly string Dummy = "I hate code that is not working as intended... I am clearly in the wrong profession!";
+
+    [Test]
+    public void IterEmpty()
     {
-        public static readonly string Dummy = "I hate code that is not working as intended... I am clearly in the wrong profession!";
+        string? v = null;
+        var arr = v.AsSpan().Split(' ').ToArray();
+        Assert.AreEqual(Array.Empty<string>(), arr);
+    }
 
-        [Test]
-        public void IterEmpty()
+    [Test]
+    public void IterSoloTest()
+    {
+        var arr = Dummy.AsSpan().Split(' ').ToArray();
+        var probe = Dummy.Split(' ').ToArray();
+        Assert.AreEqual(probe, arr);
+    }
+
+    [Test]
+    public void IterDoubleTest()
+    {
+        var sep = Dummy.AsSpan().Split(" .".AsSpan());
+        using Rustic.Memory.PoolBufWriter<string> buf = new();
+        while (sep.MoveNext())
         {
-            string? v = null;
-            var arr = v.AsSpan().Split(' ').ToArray();
-            Assert.AreEqual(Array.Empty<string>(), arr);
+            buf.Add(sep.Current.ToString());
         }
 
-        [Test]
-        public void IterSoloTest()
-        {
-            var arr = Dummy.AsSpan().Split(' ').ToArray();
-            var probe = Dummy.Split(' ').ToArray();
-            Assert.AreEqual(probe, arr);
-        }
+        var arr = buf.ToArray();
+        var probe = Dummy.Split(' ', '.').ToArray();
+        Assert.AreEqual(probe, arr);
+    }
 
-        [Test]
-        public void IterDoubleTest()
-        {
-            var sep = Dummy.AsSpan().Split(" .".AsSpan());
-            using Rustic.Memory.PoolBufWriter<string> buf = new();
-            while (sep.MoveNext())
-            {
-                buf.Add(sep.Current.ToString());
-            }
+    [Test]
+    public void IterTripleTest()
+    {
+        var arr = Dummy.AsSpan().Split(" .-".AsSpan()).ToArray();
+        var probe = Dummy.Split(' ', '.', '-').ToArray();
+        Assert.AreEqual(probe, arr);
+    }
 
-            var arr = buf.ToArray();
-            var probe = Dummy.Split(' ', '.').ToArray();
-            Assert.AreEqual(probe, arr);
-        }
+    [Test]
+    public void IterRemoveEmptyTest()
+    {
+        var arr = Dummy.AsSpan().Split('.', SplitOptions.RemoveEmptyEntries).ToArray();
+        var probe = Dummy.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        Assert.AreEqual(probe, arr);
+    }
+    [Test]
+    public void IterIncludeSeparatorsTest()
+    {
+        var arr = Dummy.AsSpan().Split(' ', SplitOptions.IncludeSeparator).ToArray();
+        var probe = Dummy.Split(' ').ToArray();
+        InsertSeparators(probe);
+        Assert.AreEqual(probe, arr);
+    }
 
-        [Test]
-        public void IterTripleTest()
+    private static void InsertSeparators(string[] array)
+    {
+        var last = array.Length - 1;
+        for (var i = 0; i < last; i += 1)
         {
-            var arr = Dummy.AsSpan().Split(" .-".AsSpan()).ToArray();
-            var probe = Dummy.Split(' ', '.', '-').ToArray();
-            Assert.AreEqual(probe, arr);
-        }
-
-        [Test]
-        public void IterRemoveEmptyTest()
-        {
-            var arr = Dummy.AsSpan().Split('.', SplitOptions.RemoveEmptyEntries).ToArray();
-            var probe = Dummy.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-            Assert.AreEqual(probe, arr);
-        }
-        [Test]
-        public void IterIncludeSeparatorsTest()
-        {
-            var arr = Dummy.AsSpan().Split(' ', SplitOptions.IncludeSeparator).ToArray();
-            var probe = Dummy.Split(' ').ToArray();
-            InsertSeparators(probe);
-            Assert.AreEqual(probe, arr);
-        }
-
-        private static void InsertSeparators(string[] array)
-        {
-            var last = array.Length - 1;
-            for (var i = 0; i < last; i += 1)
-            {
-                array[i] += " ";
-            }
+            array[i] += " ";
         }
     }
 }
