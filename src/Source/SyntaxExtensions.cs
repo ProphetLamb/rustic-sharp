fc3c2a3c3b617573bff27bb2dcada1a17b037ad1
Prophet Lamb <prophet.lamb@gmail.com>
2023-01-26 20:44:14 +0100

    Docs

--- src/Source/SyntaxExtensions.cs
+++ src/Source/SyntaxExtensions.cs
@@ -9,9 +9,15 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Rustic.Source;
 
+/// <summary>Syntax extensions.</summary>
 [CLSCompliant(false)]
 public static class SyntaxExtensions
 {
+    /// <summary>Attempts to find the a attribute attached to a <see cref="MemberDeclarationSyntax"/> node.</summary>
+    /// <param name="member">The member declaration.</param>
+    /// <param name="predicate">The function determining whether an attribute matches, or not.</param>
+    /// <typeparam name="M">The type of the <see cref="MemberDeclarationSyntax"/>.</typeparam>
+    /// <returns>The first <see cref="AttributeSyntax"/> found, if any.</returns>
     public static AttributeSyntax? FindMemAttr<M>(this SynModel<M> member, Func<SynModel<M>, AttributeSyntax, bool> predicate)
         where M : MemberDeclarationSyntax
     {
@@ -29,6 +35,11 @@ public static class SyntaxExtensions
         return null;
     }
 
+    /// <summary>Attempts to find the a attribute attached to a <see cref="BaseTypeDeclarationSyntax"/> node.</summary>
+    /// <param name="type">The member declaration.</param>
+    /// <param name="predicate">The function determining whether an attribute matches, or not.</param>
+    /// <typeparam name="T">The type of the <see cref="BaseTypeDeclarationSyntax"/>.</typeparam>
+    /// <returns>The first <see cref="AttributeSyntax"/> found, if any.</returns>
     public static AttributeSyntax? FindTypeAttr<T>(this SynModel<T> type, Func<SynModel<T>, AttributeSyntax, bool> predicate)
         where T : BaseTypeDeclarationSyntax
     {
@@ -46,6 +57,10 @@ public static class SyntaxExtensions
         return null;
     }
 
+    /// <summary>Determines the trace from the node to the <see cref="NamespaceDeclarationSyntax"/>, by traversing the parents of the node.</summary>
+    /// <param name="node">The node</param>
+    /// <typeparam name="P">The type of parents allowed in the trace. Throws if the type of any parent doesnt match.</typeparam>
+    /// <exception cref="InvalidOperationException">The type of a node is not assignable to <typeparamref name="P"/>.</exception>
     public static (NamespaceDeclarationSyntax, ImmutableArray<P>) GetHierarchy<P>(this CSharpSyntaxNode node)
         where P : MemberDeclarationSyntax
     {
@@ -68,6 +83,9 @@ public static class SyntaxExtensions
         throw new InvalidOperationException("No namespace declaration found.");
     }
 
+    /// <summary>Returns the type name of the node inside the model.</summary>
+    /// <param name="model">The model</param>
+    /// <param name="node">The node in the scope of the model.</param>
     public static string? GetTypeName(this SemanticModel model, SyntaxNode node)
     {
         // Are we a type?
@@ -87,6 +105,12 @@ public static class SyntaxExtensions
         return decl?.ToDisplayString();
     }
 
+    /// <summary>Traverses all roots inside the <see cref="Compilation"/>.</summary>
+    /// <param name="comp">The compilation to filter.</param>
+    /// <param name="predicate">The filter for relevant nodes.</param>
+    /// <param name="transform">The final transformation to apply to relevant nodes.</param>
+    /// <typeparam name="T">The type of elements produces by the transformation.</typeparam>
+    /// <returns>All filtered and transformed nodes inside any root inside the <see cref="Compilation"/>.</returns>
     public static IEnumerable<T> CollectSyntax<T>(this Compilation comp, Func<SyntaxNode, CancellationToken, bool> predicate, Func<Compilation, SyntaxNode, CancellationToken, T> transform)
     {
         foreach (var tree in comp.SyntaxTrees)
